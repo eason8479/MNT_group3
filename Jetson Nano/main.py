ca20029a34133ae2cb238d3e1141089d124a37cb
LAPTOP-L53CFE5A\KirdenKe <kedengxian@gmail.com>
2023-12-18 23:03:05 +0900

    Debug JetsonNano v3-dev
    新增查詢功能以及繼電器腳位輸出。
    Add Inquiry and Relay Pin Outputs.

--- Jetson Nano/main.py
+++ Jetson Nano/main.py
@@ -3,11 +3,32 @@ sys.path.insert(0, "..")
 import time
 from mpu6050 import mpu6050
 from mlx90614 import MLX90614
+import json
+import requests
 import Jetson.GPIO as GPIO
 
 from opcua import ua, Server
 
+def showResult(angle_data, nozzle_data, plate_data):
+    json_inputs = json.dumps({'input': [angle_data['x'], angle_data['y'], nozzle_data, plate_data]})
+    response = requests.post(url_log, json=json_inputs)
+    if response.json().index(max(response.json())) == 0:
+        GPIO.output(output_pin, GPIO.LOW)
+        predict_data.set_value(True)
+    else:
+        GPIO.output(output_pin, GPIO.HIGH)
+        predict_data.set_value(False)
+
 if __name__ == "__main__":
+
+    url_log = 'http://192.168.50.168:25926/inference'
+
+    # set mode (BOARD/BCM)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    # sensor out
+    output_pin = 18
+    GPIO.setup(output_pin, GPIO.OUT)
 
     # setup our server
     server = Server()
@@ -27,10 +48,13 @@ if __name__ == "__main__":
     myobj_temp = objects.add_object(idx, "temp")
     nozzle_temp = myobj_temp.add_variable(idx, "nozzle_temp", 0.1)
     plate_temp = myobj_temp.add_variable(idx, "plate_temp", 0.1)
+    myobj_predict = objects.add_object(idx, "predict")
+    predict_data = myobj_predict.add_variable(idx, "isSuccessfully", True)
     angle_x.set_writable()
     angle_y.set_writable()
     nozzle_temp.set_writable()
     plate_temp.set_writable()
+    predict_data.set_writable()
 
     # starting!
     server.start()
@@ -42,10 +66,13 @@ if __name__ == "__main__":
         while (True):
             time.sleep(1)
             angle_data = mpu.get_angle_data()
+            nozzle_data = sensor.get_obj_temp()
+            plate_data = plateSensor.get_obj_temp()
             angle_x.set_value(angle_data['x'])
             angle_y.set_value(angle_data['y'])
-            nozzle_temp.set_value(sensor.get_obj_temp())
-            plate_temp.set_value(plateSensor.get_obj_temp())
+            nozzle_temp.set_value(nozzle_data)
+            plate_temp.set_value(plate_data)
+            showResult(angle_data, nozzle_data, plate_data)
 
     finally:
         # close connection, remove subcsriptions, etc
